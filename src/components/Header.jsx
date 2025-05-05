ad73c968cd3830b93dcc16a86700edf3f37cfabb
Damilola Joshua <damilolajoshua2021@gmail.com>
2025-05-04 21:03:02 -0700

    Finished with Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,16 @@ import { Menu, User } from 'lucide-react'
 
 const Header = () => {
   return (
-    <div className='flex justify-between items-center'>
+    <div className='flex justify-between items-center pl-4 pr-4'>
       <Menu size={50} />
       <div className='text-6xl'>
         Dev Diary
       </div>
-      <div className='flex justify-center items-center'>
+      <div className='flex justify-between items-center w-40'>
         <div className=' text-3xl'>
           Dammy
         </div>
-        <div className='bg-gray-300 rounded-full'>
+        <div className='bg-gray-300 w-12 h-12 rounded-full flex justify-center items-center'>
           <User />
         </div>
       </div>
